f03458131dc7e821db4d09adbb5904a4169e3c19
Guilherme Gatto <guuilhermegatto@hotmail.com>
2024-05-20 12:17:05 -0300

    initial project

---
+++ CI-CD/CI-CD/CI_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CDApp.swift
+//  CI-CD
+//
+//  Created by Guilherme Gatto on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CI_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI-CD/CI-CD/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CI-CD
+//
+//  Created by Guilherme Gatto on 20/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
