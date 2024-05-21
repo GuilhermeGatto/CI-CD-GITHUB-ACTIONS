ac937d4db124137d942ec87e3088bd61bc4a25e6
Guilherme Gatto <guuilhermegatto@hotmail.com>
2024-05-21 18:22:09 -0300

    fix lint

--- CI-CD/CI-CDTests/CI_CDTests.swift
+++ CI-CD/CI-CDTests/CI_CDTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class CI_CDTests: XCTestCase {
+final class CICDTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
