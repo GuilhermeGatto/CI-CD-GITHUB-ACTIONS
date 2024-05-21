8b40aa115d7748d350cdb1a4ed10a77036198e5b
Guilherme Gatto <guuilhermegatto@hotmail.com>
2024-05-21 19:19:03 -0300

    test

---
+++ CI-CD/CI-CDTests/CICDTests.swift
@@ -0,0 +1,24 @@
+//
+//  CI_CDTests.swift
+//  CI-CDTests
+//
+//  Created by Guilherme Gatto on 21/05/24.
+//
+
+import XCTest
+@testable import CI_CD
+final class CICDTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual("a", "a")
+    }
+
+}
